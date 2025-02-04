9e5b91c6488ed32d8edbb355a4c8ef2c0c16fea5
ndeyerokhayandiaye <rokhayandeye8@gamil.com>
2025-02-04 21:59:33 +0000

    Debut interface accueil

--- src/app/accueil/accueil.component.ts
+++ src/app/accueil/accueil.component.ts
@@ -3,13 +3,17 @@ import { Component } from '@angular/core';
 // import { HeaderComponent } from '../components/header/header.component';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../header/header.component';
+import { LoginComponent } from '../login/login.component';
+import { RouterModule } from '@angular/router';
 
 
 @Component({
   selector: 'app-accueil',
   standalone: true,
   imports: [CommonModule,
-    HeaderComponent
+    HeaderComponent,
+    LoginComponent,
+    RouterModule
   ],
   templateUrl: './accueil.component.html',
   styleUrl: './accueil.component.scss'
