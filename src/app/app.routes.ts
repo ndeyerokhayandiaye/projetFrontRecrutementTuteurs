e82676cc2d92fdb9dbfa94682e8ec8d9141237a7
ndeyerokhayandiaye <rokhayandeye8@gamil.com>
2025-02-05 21:20:45 +0000

    interface annonce et profil

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,14 @@ import { Routes } from '@angular/router';
 import { AccueilComponent } from './accueil/accueil.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-import { HeaderComponent } from './header/header.component';
+import { AnnonceComponent } from './annonce/annonce.component';
+import { ProfilComponent } from './profil/profil.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'accueil', pathMatch: 'full' },
   { path: 'accueil', component: AccueilComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'header', component: HeaderComponent },
+  { path: 'annonce', component: AnnonceComponent },
+  { path: 'profil', component: ProfilComponent},
 ];
