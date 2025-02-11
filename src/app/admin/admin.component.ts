a229f2889a7c2f06dffd6496a9be8adfd41795f2
ndeyerokhayandiaye <rokhayandeye8@gamil.com>
2025-02-11 23:30:16 +0000

    after error datatable

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -3,15 +3,11 @@ import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { RouterModule } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { BrowserModule } from '@angular/platform-browser';
-// import { DataTablesModule } from 'angular-datatables';
 
 @Component({
   selector: 'app-admin',
   standalone: true,
   imports: [CommonModule, SidebarComponent, RouterModule, NavbarComponent,
-    BrowserModule,
-    // DataTablesModule
   ],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.scss',
--- src/app/admin/components/annonce-list/annonce-list.component.ts
+++ src/app/admin/components/annonce-list/annonce-list.component.ts
@@ -1,28 +1,13 @@
-import { CommonModule,  } from '@angular/common';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
+import { Component } from '@angular/core';
 
 
 @Component({
   selector: 'app-annonce-list',
   standalone: true,
-  imports: [
-
-    FormsModule,
-    BrowserModule,
-    HttpClientModule,
-    FormsModule,
-    CommonModule,
-    RouterModule,
-  ],
+  imports: [],
   templateUrl: './annonce-list.component.html',
   styleUrl: './annonce-list.component.scss'
 })
 export class AnnonceListComponent  {
-
-
 
 }
